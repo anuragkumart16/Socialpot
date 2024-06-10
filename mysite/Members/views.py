d23936da4803a8c53111849319640a3aee293e5e
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-06-10 08:45:50 +0300

    added things

--- mysite/Members/views.py
+++ mysite/Members/views.py
@@ -61,11 +61,3 @@ def logoutview(request):
 
 # views.py
 
-from django.http import JsonResponse
-
-def check_username(request):
-    username = request.GET.get('username', None)
-    data = {
-        'is_taken': User.objects.filter(username__iexact=username).exists()
-    }
-    return JsonResponse(data)
