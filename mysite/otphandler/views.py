b09c52d4b3a9462cf7183ba909709347a0a5fb31
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-06-27 06:59:13 +0300

    otp is working now

--- mysite/otphandler/views.py
+++ mysite/otphandler/views.py
@@ -19,7 +19,7 @@ class otprequest(APIView):
         if not serializer.is_valid():
             return Response({
                 'status': False,
-                'message' : 'something went wrong'
+                'message' : 'something went wrong and its from frontend'
             },status.HTTP_400_BAD_REQUEST)
         
         email = data['email']
@@ -27,8 +27,8 @@ class otprequest(APIView):
         otp= random.randint(0000,9999)
 
         # Email account credentials
-        from_address = "vipultiwaribsdk@gmail.com"
-        password = "redminote7pro"
+        from_address = "acrossdevice01@gmail.com"
+        password = "bapw oify vutv fuau"
 
         # send email to 
         to_address = email
@@ -52,6 +52,12 @@ class otprequest(APIView):
         server.login(from_address, password)
         server.sendmail(from_address, to_address, msg.as_string())
         server.quit()
+
+        return Response({
+            'status':True,
+            'message':'otp succesfully sent',
+            'otp': f'{otp}'
+        },status.HTTP_200_OK)
 
 
 
