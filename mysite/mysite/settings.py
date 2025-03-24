a7b442a90e245705a921e4926472d3fea43def54
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-03-24 15:00:27 +0530

    added localhost and vercel app to server

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -149,6 +149,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "https://acrossdevice.github.io",
     'http://192.168.1.3:5500',
+    'http://localhost:5173',
+    'https://across-device.vercel.app',
 ]
 from datetime import timedelta
 
